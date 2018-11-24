3dd3d6a53aa05ed345c8afaa548d394db4815e4b
KellenNjoroge <muthonkel@gmail.com>
2018-11-24 16:14:38 +0300

    first migration and model

--- jirani/models.py
+++ jirani/models.py
@@ -22,91 +22,91 @@ class Hood(models.Model):
         return hood
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    bio = models.TextField(max_length=100, blank=True)
-    profilepic = models.ImageField(upload_to='picture/', blank=True)
-    contact = models.CharField(max_length=15, blank=True)
-    hoodpin = models.BooleanField(default=False)
-    hood = models.ForeignKey(Hood, related_name='home', null=True)
-
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
-
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
-
-    def __str__(self):
-        return self.user.username
-
-
-# class Hood(models.Model):
-#     name = models.CharField(max_length=20,unique=True)
-#     residents = models.IntegerField(default=1)
-#     county = models.CharField(max_length=20)
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+#     bio = models.TextField(max_length=100, blank=True)
+#     profilepic = models.ImageField(upload_to='picture/', blank=True)
+#     contact = models.CharField(max_length=15, blank=True)
+#     hoodpin = models.BooleanField(default=False)
+#     hood = models.ForeignKey(Hood, related_name='home', null=True)
 #
-#     def save_hood(self):
+#     @receiver(post_save, sender=User)
+#     def create_user_profile(sender, instance, created, **kwargs):
+#         if created:
+#             Profile.objects.create(user=instance)
+#
+#     @receiver(post_save, sender=User)
+#     def save_user_profile(sender, instance, **kwargs):
+#         instance.profile.save()
+#
+#     def __str__(self):
+#         return self.user.username
+#
+#
+# # class Hood(models.Model):
+# #     name = models.CharField(max_length=20,unique=True)
+# #     residents = models.IntegerField(default=1)
+# #     county = models.CharField(max_length=20)
+# #
+# #     def save_hood(self):
+# #         self.save()
+# #
+# #     def remove_hood(self):
+# #         self.delete()
+# #
+# #
+# #     @classmethod
+# #     def get_hood(cls,id):
+# #         hood = Hood.objects.get(id=id)
+# #         return hood
+#
+# class Post(models.Model):
+#     title = models.CharField(max_length=30)
+#     post = models.TextField(max_length=100)
+#     hood = models.ForeignKey(Hood, related_name='hood')
+#     poster = models.ForeignKey(User, on_delete=models.CASCADE)
+#
+#     def save_post(self):
 #         self.save()
 #
-#     def remove_hood(self):
+#     def remove_post(self):
+#         self.delete()
+#
+#     @classmethod
+#     def get_hood_posts(cls, id):
+#         posts = Post.objects.filter(id=id)
+#         return posts
+#
+#
+# class Comment(models.Model):
+#     comment = models.CharField(max_length=100)
+#     commentator = models.ForeignKey(User)
+#     comment_post = models.ForeignKey(Post, related_name='comment', null=True)
+#
+#     def save_comment(self):
+#         self.save()
+#
+#     def delete_comment(self):
 #         self.delete()
 #
 #
+# class Business(models.Model):
+#     name = models.CharField(max_length=20)
+#     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+#     business_description = models.TextField(max_length=80)
+#     locale = models.ForeignKey(Hood, related_name='location')
+#     # category = models.CharField(max_length=20)
+#     business_number = models.IntegerField(default=0)
+#
+#     def save_business(self):
+#         self.save()
+#
+#     def delete_business(self):
+#         self.delete()
+#
 #     @classmethod
-#     def get_hood(cls,id):
-#         hood = Hood.objects.get(id=id)
-#         return hood
-
-class Post(models.Model):
-    title = models.CharField(max_length=30)
-    post = models.TextField(max_length=100)
-    hood = models.ForeignKey(Hood, related_name='hood')
-    poster = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def save_post(self):
-        self.save()
-
-    def remove_post(self):
-        self.delete()
-
-    @classmethod
-    def get_hood_posts(cls, id):
-        posts = Post.objects.filter(id=id)
-        return posts
-
-
-class Comment(models.Model):
-    comment = models.CharField(max_length=100)
-    commentator = models.ForeignKey(User)
-    comment_post = models.ForeignKey(Post, related_name='comment', null=True)
-
-    def save_comment(self):
-        self.save()
-
-    def delete_comment(self):
-        self.delete()
-
-
-class Business(models.Model):
-    name = models.CharField(max_length=20)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    business_description = models.TextField(max_length=80)
-    locale = models.ForeignKey(Hood, related_name='location')
-    # category = models.CharField(max_length=20)
-    business_number = models.IntegerField(default=0)
-
-    def save_business(self):
-        self.save()
-
-    def delete_business(self):
-        self.delete()
-
-    @classmethod
-    def get_business(cls, name):
-        business = Business.objects.filter(name=name)
-        return business
-
-    # def update_business(self,id):
+#     def get_business(cls, name):
+#         business = Business.objects.filter(name=name)
+#         return business
+#
+#     # def update_business(self,id):
--- keller/settings.py
+++ keller/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jirani',
+    'bootstrap4',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,10 @@ WSGI_APPLICATION = 'keller.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jirani',
+        'USER': 'kellen',
+        'PASSWORD': 'kellen',
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -118,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
